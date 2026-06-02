3a153960b467dd160288d761ba990705c607bd23
izzzi <izzzilolrblx@gmail.com>
2026-06-02 13:24:07 +0300

    Revert "REFACTOOR!"
    This reverts commit d3cd26e3e1e80f528eec69eb881ee1cca44b99b5.

--- js/terminal/cmd_get.js
+++ js/terminal/cmd_get.js
@@ -54,7 +54,7 @@ const CmdGet = {
             // sessionStorage недоступен — просто игнорируем
         }
 
-        const url = `https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/contents/${DOSSIERS_ROOT}?ref=DossiersBase`;
+        const url = `https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/contents/${DOSSIERS_ROOT}`;
 
         try {
             const response = await fetch(url, { headers: this._apiHeaders() });
